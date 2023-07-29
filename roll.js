4f20d9c9f977507d3233e654a23c3db3e13f43aa
joshDev_ <joshua.siwan@outlook.com>
2023-07-29 14:18:25 +1000

    roll functionality.I have used an if condition to randomly generate  value base don whether a dice with 6 or 10 faces has been rolled

---
+++ roll.js
@@ -0,0 +1,12 @@
+function diceRoll ( dice ) {
+    if (dice==6) {
+       return Math.floor(Math.random() * (6 - 1 + 1) + 1)
+    }
+        if (dice==10) {
+       return Math.floor(Math.random() * (10 - 1 + 1) + 1)
+    }
+}
+
+
+
+
